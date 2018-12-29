4c518c5d9ac46d3edc95e7b8592bde37f49d3dc5
Marco Gomiero <marco.gomiero.93@gmail.com>
2018-12-29 14:15:35 +0100

    remove banner and fix versioning

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class BMBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: _title,
       theme: ThemeData(
         primarySwatch: Colors.blue,
